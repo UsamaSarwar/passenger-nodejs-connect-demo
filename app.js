bd3403dae11d9487aa5cd9e25191687fc2514ce9
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2022-08-02 17:19:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ http.createServer(app).listen(3000);
 if (typeof(PhusionPassenger) !== 'undefined') {
   console.log('Example app running inside Passenger.');
 } else {
-  console.log('Example app listening on http://127.0.0.1:3000/');
+  console.log('Example app listening on http://3.111.187.78:3000/');
 }
